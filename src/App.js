998dfbeda85c50d24443afc0bf50a9ce06e793df
Ajayp111 <126410871+Ajayp111@users.noreply.github.com>
2023-10-01 14:32:49 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import { useRef, useState } from "react";
+import CustomForm from "./components/CustomForm/CustomForm";
 
-function App() {
+export default function App() {
+  const usernameRef = useRef();
+  const passwordRef = useRef();
+  const handleLogin = () => {
+    console.log("login");
+    console.log(usernameRef.current.value);
+    console.log(passwordRef.current.value);
+    //make a login api call
+  };
+  const handleRegister = () => {
+    console.log("register");
+    console.log(usernameRef.current.value);
+    console.log(passwordRef.current.value);
+    //make a register api call
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <CustomForm
+        username={{
+          type: "text",
+          label: "Username",
+          placeholder: "Enter Username",
+          ref: usernameRef,
+          isInput: true,
+        }}
+        password={{
+          type: "password",
+          label: "Password",
+          placeholder: "Enter Password",
+          ref: passwordRef,
+          isInput: true,
+        }}
+        loginButton={{
+          handler: handleLogin,
+          buttonText: "Login",
+          isInput: false,
+        }}
+        registerButton={{
+          handler: handleRegister,
+          buttonText: "Register",
+          isInput: false,
+        }}
+      />
     </div>
   );
 }
-
-export default App;
